4e69d54238cc5241d7d4092eacdca8c90088d21d
Ali Soltani <soltani1384@gmail.com>
2020-08-20 16:29:01 +0430

    clean code

--- structure-sample/src/app/app.module.ts
+++ structure-sample/src/app/app.module.ts
@@ -2,24 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// import { CoreModule } from './core/core.module';
+import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialCollectionModule } from './material-collection/material-collection.module';
+import { FooterComponent } from './core/layout/footer/footer.component';
 // import { MatSliderModule } from '@angular/material/slider';
-
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    // CoreModule,
+    CoreModule,
     MaterialCollectionModule,
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
--- structure-sample/src/app/core/core.module.ts
+++ structure-sample/src/app/core/core.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { MaterialCollectionModule } from ".././material-collection/material-collection.module";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
-    MaterialCollectionModule
+    MaterialCollectionModule,
+    HttpClientModule,
   ],
-  exports: [],
+  exports: [
+    HeaderComponent
+  ],
   providers: [],
 })
 export class CoreModule { }
