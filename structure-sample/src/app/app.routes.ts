6f162320da6837a542434fd87e79a7c6c63e8caa
Ali Soltani <soltani1384@gmail.com>
2020-08-27 20:46:51 +0430

    adding app routes

---
+++ structure-sample/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./core/index";
+
+export const appRoutes: Routes  = [
+  {
+    path: "", component: HomeComponent
+  }
+]
--- structure-sample/src/app/app.routing.module.ts
+++ structure-sample/src/app/app.routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
+    imports:[RouterModule.forChild(appRout)]
 
 })
 export class AppRoutingModule { }
