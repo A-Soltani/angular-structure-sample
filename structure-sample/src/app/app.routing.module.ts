e09fd8b75fe68d405851e79a4a76acaa5e10c823
Ali Soltani <soltani1384@gmail.com>
2020-08-28 05:11:25 +0430

    adding route module

--- structure-sample/src/app/app.routing.module.ts
+++ structure-sample/src/app/app.routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from '@angular/router';
+import { appRoutes } from "./app.routes";
 
 
 @NgModule({
-    imports:[RouterModule.forChild(appRout)]
+    imports:[RouterModule.forChild(appRoutes)],
+    exports: [RouterModule]
 
 })
 export class AppRoutingModule { }
