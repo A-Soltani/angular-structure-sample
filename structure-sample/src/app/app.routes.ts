91b3ff375ec688fda67517ffcaa8e7602aad2814
Ali Soltani <soltani1384@gmail.com>
2020-08-30 20:47:24 +0430

    adding a new route for currency module in app.routes

--- structure-sample/src/app/app.routes.ts
+++ structure-sample/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./core/index";
 
-export const appRoutes: Routes  = [
+export const appRoutes: Routes = [
   {
     path: "", component: HomeComponent
+  },
+  {
+    path: "currency",
+    loadChildren: () => import('./currency/currency.module').then(m => m.CurrencyModule)
   }
 ]
