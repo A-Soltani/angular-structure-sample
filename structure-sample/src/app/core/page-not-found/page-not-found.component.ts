cecbdcde833254fddf1975528460890c02af71a6
Ali Soltani <soltani1384@gmail.com>
2020-08-30 21:12:24 +0430

    fixing bug in PageNotFoundComponent

--- structure-sample/src/app/core/page-not-found/page-not-found.component.ts
+++ structure-sample/src/app/core/page-not-found/page-not-found.component.ts
@@ -1,8 +1,12 @@
+import { Component } from "@angular/core";
+
 @Component({
     selector: 'app-page-not-found',
     templateUrl: './page-not-found.component.html',
-    styleUrl: ['./page-not-found.component.css']
+    styleUrls: ['./page-not-found.component.css']
 })
-export calss PageNotFoundComponent {
+
+export class PageNotFoundComponent {
 
 }
+
