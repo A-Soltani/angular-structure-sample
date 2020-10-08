4b41f79a9fd07b925748ad04973cc06dfcfd24b7
Ali Soltani <soltani1384@gmail.com>
2020-10-08 23:04:17 +0330

    fixing curreny-edit test

--- structure-sample/src/app/currencies/currency-edit/currency-edit.component.spec.ts
+++ structure-sample/src/app/currencies/currency-edit/currency-edit.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 import { CurrencyEditComponent } from './currency-edit.component';
+
 
 describe('CurrencyEditComponent', () => {
   let component: CurrencyEditComponent;
@@ -8,7 +12,12 @@ describe('CurrencyEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CurrencyEditComponent ]
+      declarations: [ CurrencyEditComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
+
     })
     .compileComponents();
   }));
@@ -22,4 +31,8 @@ describe('CurrencyEditComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+// it('should create a form contains 5 contols', () => {
+//   expect(component).toContain('code');
+// })
 });
