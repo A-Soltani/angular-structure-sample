e26a1357f885db3e961d4ebf966dee0475f2b16f
Ali Soltani <soltani1384@gmail.com>
2020-10-20 21:21:32 +0330

    adding unit test for app routing

---
+++ structure-sample/src/app/app.module.spec.ts
@@ -0,0 +1,37 @@
+import { Location } from "@angular/common";
+import { APP_Routes } from './app.routes';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, getTestBed, fakeAsync, tick } from '@angular/core/testing';
+import { Router } from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './core';
+
+describe('Router: App', () => {
+  let location: Location;
+  let router: Router;
+  let fixture;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(APP_Routes)
+      ],
+      declarations: [
+        HomeComponent,
+        AppComponent
+      ]
+    });
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
+  });
+
+  it('navigate to "" redirects to /home', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('/home');
+  }))
+});
--- structure-sample/src/app/app.routes.ts
+++ structure-sample/src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from "@angular/router";
+
 import { HomeComponent, PageNotFoundComponent } from "./core/index";
 
 export const APP_Routes: Routes = [
-  { path: "", component: HomeComponent },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
   {
     path: "currencies",
     loadChildren: () => import('./currencies').then(m => m.CurrenciesModule)
   },
-  { path: '**', component: PageNotFoundComponent } // Wildcard route for a 404 page
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  } // Wildcard route for a 404 page
 ]
