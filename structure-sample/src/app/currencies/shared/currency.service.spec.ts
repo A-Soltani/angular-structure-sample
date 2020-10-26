94797e0c6d9b4068e865f1ff9e59041997cdb4aa
Ali Soltani <soltani1384@gmail.com>
2020-10-26 23:02:25 +0330

    mocking http client in currency service

--- structure-sample/src/app/currencies/shared/currency.service.spec.ts
+++ structure-sample/src/app/currencies/shared/currency.service.spec.ts
@@ -1,10 +1,12 @@
+import { Currency } from './currency.model';
 import { HttpClient } from '@angular/common/http';
 import { inject, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CurrencyService } from "./currency.service";
+import { of } from 'rxjs';
 
 describe('CurrencyService', () => {
-  let httpClient: HttpClient;
+  let httpClientSpy: { get: jasmine.Spy };
   let currencyService: CurrencyService;
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,12 +14,26 @@ describe('CurrencyService', () => {
       providers: [CurrencyService]
     });
 
-    httpClient = TestBed.inject(HttpClient);
-    currencyService = TestBed.inject(CurrencyService);
+    // TODO: spy on other methods too
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    currencyService = new CurrencyService(httpClientSpy as any);
   })
 
   it('should create Currency Service', () => {
     expect(currencyService).toBeTruthy();
+  });
+
+  it('should return expected currencies (HttpClient called once)', () => {
+    const expectedCurrencies: Currency[] =
+      [new Currency()];
+
+    httpClientSpy.get.and.returnValue(of(expectedCurrencies));
+
+    currencyService.getCurrencies().subscribe(
+      currencies => expect(currencies).toEqual(expectedCurrencies, 'expected currencies'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 
 });
