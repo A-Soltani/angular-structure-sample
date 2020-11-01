1bddaf6152f096ff74c4fcd72be796793ea4120b
Ali Soltani <soltani1384@gmail.com>
2020-11-01 21:45:59 +0330

    adding some config for error interceptor

--- structure-sample/src/app/core/interceptors/error.interceptor.spec.ts
+++ structure-sample/src/app/core/interceptors/error.interceptor.spec.ts
@@ -1,4 +1,56 @@
+import { ErrorInterceptor } from './error.interceptor';
+import { HttpClient, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { Observable, throwError } from "rxjs";
 
 describe('ErrorInterceptor', () => {
+  let httpRequestSpy;
+  let httpHandlerSpy;
+  const error = { status: 400, statusText: 'error' };
+  let errorInterceptor: ErrorInterceptor;
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: ErrorInterceptor,
+          multi: true
+        },
+        ErrorInterceptor
+      ]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+    errorInterceptor = TestBed.inject(ErrorInterceptor);
+    errorInterceptor = new ErrorInterceptor()
+  });
+
+  it('should return BadInput if 400 response is returned from API', () => {
+    //arrange
+    httpRequestSpy = jasmine.createSpyObj('HttpRequest', ['doesNotMatter']);
+    httpHandlerSpy = jasmine.createSpyObj('HttpHandler', ['handle']);
+    httpHandlerSpy.handle.and.returnValue(throwError(
+      {
+        error:
+          { message: 'test-error' }
+      }
+    ));
+
+    // errorInterceptor.intercept(httpRequestSpy, httpHandlerSpy)
+    //   .subscribe(
+    //     result => console.log('good', result),
+    //     err => {
+    //       console.log('error', err);
+    //       expect(err).toEqual('test-error');
+    //     }
+    //   );
+
+  });
 
 });
