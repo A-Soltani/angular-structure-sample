134d127b9fbdf8f47310b1e06f8812327b3d0467
Ali Soltani <soltani1384@gmail.com>
2020-11-08 22:23:10 +0330

    adding a failure test for customValidationService

--- structure-sample/src/app/core/services/custom-validation.service.spec.ts
+++ structure-sample/src/app/core/services/custom-validation.service.spec.ts
@@ -1,3 +1,4 @@
+import { AbstractControl } from '@angular/forms';
 import { TestBed } from "@angular/core/testing";
 import { CustomvalidationService } from "./custom-validation.service";
 
@@ -11,5 +12,12 @@ describe('CustomValidationService', () => {
 
   it('Should be created', () => {
     expect(customValidationService).toBeTruthy();
+  });
+
+  it('Should return false when value of a control contains space', () => {
+    let control: AbstractControl;
+    control.setValue('test ');
+    var validation = customValidationService.canNotContainSpace(control);
+    expect(validation).toBeFalse();
   });
 });
