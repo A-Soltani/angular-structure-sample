86d42247d7679f788966f66e57ccc3ee618ee45a
Ali Soltani <soltani1384@gmail.com>
2020-11-10 21:53:12 +0330

    adding a new test for PageNotFoundComponent

--- structure-sample/src/app/core/page-not-found/page-not-found.component.spec.ts
+++ structure-sample/src/app/core/page-not-found/page-not-found.component.spec.ts
@@ -4,16 +4,26 @@ import { PageNotFoundComponent } from "./page-not-found.component";
 
 describe('PageNotFoundComponent', () => {
   let pageNotFoundComponent: PageNotFoundComponent;
+  let fixture;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PageNotFoundComponent]
     });
+
+    fixture = TestBed.createComponent(PageNotFoundComponent);
+    pageNotFoundComponent = fixture.componentInstance;
   }));
 
   it('Should create the component', () => {
-    const fixture = TestBed.createComponent(PageNotFoundComponent);
-    pageNotFoundComponent = fixture.componentInstance;
     expect(pageNotFoundComponent).toBeTruthy();
+  });
+
+  it('Should contain an image for 404', () => {
+    const compiled = fixture.nativeElement;
+    let img = compiled.querySelector('img');
+    console.log();
+    expect(img.getAttribute('src') !== '').toBeTrue();
+    expect(img.getAttribute('alt')).toContain('404');
   });
 
 })
