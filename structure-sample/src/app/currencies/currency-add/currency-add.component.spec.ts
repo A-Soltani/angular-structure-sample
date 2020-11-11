c26b77ada955d9e98270c0f58844ba57230d35a2
Ali Soltani <soltani1384@gmail.com>
2020-11-11 22:24:43 +0330

    adding a new test for CurrencyAddComponent

--- structure-sample/src/app/currencies/currency-add/currency-add.component.spec.ts
+++ structure-sample/src/app/currencies/currency-add/currency-add.component.spec.ts
@@ -1,25 +1,29 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { CurrencyService } from '../shared/currency.service';
 
-// import { CurrencyAddComponent } from './currency-add.component';
+import { CurrencyAddComponent } from './currency-add.component';
 
-// describe('CurrencyAddComponent', () => {
-//   let component: CurrencyAddComponent;
-//   let fixture: ComponentFixture<CurrencyAddComponent>;
+describe('CurrencyAddComponent', () => {
+  let component: CurrencyAddComponent;
+  let fixture: ComponentFixture<CurrencyAddComponent>;
+  let currencyService: CurrencyService;
+  let router: Router;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CurrencyAddComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CurrencyAddComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CurrencyAddComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CurrencyAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
