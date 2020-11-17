20ba0a9fd3d261b9083b478497e4213104a5b722
Ali Soltani <soltani1384@gmail.com>
2020-11-17 20:40:55 +0330

    removing unnecessary barrel files

--- structure-sample/src/app/material-collection/material-collection.module.ts
+++ structure-sample/src/app/material-collection/material-collection.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatSliderModule } from '@angular/material/slider';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    MatDialogModule,
-    MatSliderModule
-  ],
   exports: [
     MatDialogModule,
     MatSliderModule
-  ],
-  providers:[
-    MatDialog
   ]
 })
 export class MaterialCollectionModule { }
