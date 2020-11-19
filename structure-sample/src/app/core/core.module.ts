c77ab13cd2042f564fc4aa5ccc6bbfcd768d4865
Ali Soltani <soltani1384@gmail.com>
2020-11-19 22:08:43 +0330

    adding flex layout to add currency

--- structure-sample/src/app/core/core.module.ts
+++ structure-sample/src/app/core/core.module.ts
@@ -2,13 +2,13 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.componet';
 
 @NgModule({
   declarations: [
@@ -26,11 +26,11 @@ import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dial
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    MatConfirmDialogComponent
   ],
 })
 export class CoreModule {
