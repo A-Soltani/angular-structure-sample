dd802fb97419ba4afb9c2bd0731542f7ba9f162a
Ali Soltani <soltani1384@gmail.com>
2020-11-25 22:00:48 +0330

    adding sidebar module into layout

--- structure-sample/src/app/layout/layout.module.ts
+++ structure-sample/src/app/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SidebarComponent],
   imports: [
     CommonModule
   ]
