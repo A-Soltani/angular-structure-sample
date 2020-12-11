df8102e288e80ecb015f37cedda7c4e70f978631
Ali Soltani <soltani1384@gmail.com>
2020-12-11 20:10:37 +0330

    adding header and footer in layout located in shared

--- structure-sample/src/app/shared/shared.module.ts
+++ structure-sample/src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent, HeaderComponent],
   imports: [ CommonModule ],
   exports: [],
   providers: [],
